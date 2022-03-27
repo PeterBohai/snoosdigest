dd4b21af7792e388f394177d1d0c6c12b779880e
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-03-27 16:22:33 -0500

    Connect to hosted PostgreSQL database instead of SQLite

--- snoosdigest/settings.py
+++ snoosdigest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'snoosdigest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME_DEV'),
+        'USER': os.environ.get('DB_USER_DEV'),
+        'PASSWORD': os.environ.get('DB_PASSWORD_DEV'),
+        'HOST': os.environ.get('DB_HOST_DEV'),
+        'PORT': '5432'
     }
 }
 
