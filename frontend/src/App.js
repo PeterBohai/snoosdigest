0c62aca0f08bbd128b62403643c69843ae93f09e
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-03-30 22:26:32 -0500

    Create basic React frontend component

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Welcome!</h1>
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <header>Snoos Digest</header>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header
