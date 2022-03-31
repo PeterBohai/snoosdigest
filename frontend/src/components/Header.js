8d282d32ee58efb051654edac3b5a5aaa2c2cd8a
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-03-30 23:05:54 -0500

    Install material ui and style Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 function Header() {
     return (
-        <div>
-            <header>Snoos Digest</header>
-            <hr/>
-        </div>
+        <AppBar position='static'>
+            <Toolbar>
+                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 1 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                    Snoos Digest
+                </Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
 
