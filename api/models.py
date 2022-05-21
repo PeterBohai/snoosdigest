276212a2b93006e09a255daddbc87b655630d885
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-05-21 17:00:00 -0500

    Add models for Subreddit and SubredditPost

--- api/models.py
+++ api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Subreddit(models.Model):
+    subreddit_id = models.AutoField(primary_key=True)
+    reddit_id = models.CharField(max_length=16)
+    display_name = models.CharField(max_length=128)
+    display_name_prefixed = models.CharField(max_length=128)
+    reddit_url_path = models.CharField(max_length=128)
+    subscribers = models.PositiveIntegerField(null=True)
+    created_date_utc = models.DateField(null=True)
+    created_unix_timestamp = models.PositiveBigIntegerField(null=True)
+    data_updated_timestamp_utc = models.DateTimeField()
+
+    class Meta:
+        db_table = 'subreddit'
+
+
+class SubredditPost(models.Model):
+    subreddit_post_id = models.AutoField(primary_key=True)
+    subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
+    reddit_id = models.CharField(max_length=16)
+    reddit_url = models.CharField(max_length=256)
+    top_day_order = models.PositiveSmallIntegerField(null=True)
+    top_week_order = models.PositiveSmallIntegerField(null=True)
+    top_month_order = models.PositiveSmallIntegerField(null=True)
+    top_year_order = models.PositiveSmallIntegerField(null=True)
+    top_all_order = models.PositiveSmallIntegerField(null=True)
+    title = models.CharField(max_length=300)
+    body = models.TextField(null=True, blank=True)
+    author_name = models.CharField(max_length=64)
+    upvotes = models.IntegerField()
+    upvote_ratio = models.FloatField(null=True)
+    num_comments = models.PositiveIntegerField()
+    img_url = models.CharField(max_length=256, null=True, blank=True)
+    video_url = models.CharField(max_length=256, null=True, blank=True)
+    over_18 = models.BooleanField(null=True)
+    spoiler = models.BooleanField(null=True)
+    created_timestamp_utc = models.DateTimeField()
+    created_unix_timestamp = models.PositiveBigIntegerField()
+    data_updated_timestamp_utc = models.DateTimeField()
+
+    class Meta:
+        db_table = 'subreddit_post'
