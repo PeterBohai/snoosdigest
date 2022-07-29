b468f44506c5e475abecec2684b542b7f60408c0
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-07-28 22:08:31 -0500

    Create and migrate UserSubscription model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from api.models import Subreddit
 
 
 class User(AbstractUser):
     pass
+
+
+class UserSubscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
+    sort_order = models.PositiveIntegerField()
+    favorite = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users_user_subscriptions'
