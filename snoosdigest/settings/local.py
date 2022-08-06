64ec6299dab0685268d0cf8596080e71ca06f5e2
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-08-05 21:41:40 -0500

    Update settings structure and add Heroku deployment files

---
+++ snoosdigest/settings/local.py
@@ -0,0 +1,1 @@
+from snoosdigest.settings.base import *
--- snoosdigest/urls.py
+++ snoosdigest/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/users/', include('users.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
