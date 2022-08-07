3dec4bc459ca8e78e731ff8f083eeddc80b1f01e
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-08-07 17:38:23 -0500

    Update Django urls to work with React Router routes

--- snoosdigest/urls.py
+++ snoosdigest/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
+    path('/', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
