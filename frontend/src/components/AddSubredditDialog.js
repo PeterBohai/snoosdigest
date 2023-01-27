eac1958bb0bb0b015452ab46c7b6f14280616fa9
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2023-01-27 16:37:48 -0600

    Update most npm packages

--- frontend/src/components/AddSubredditDialog.js
+++ frontend/src/components/AddSubredditDialog.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
