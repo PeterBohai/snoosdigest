2165afdd1c9673b688b2f89808023c55cd3a31d0
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2023-02-16 22:30:27 -0600

    Add sort by filter for hackernews frontend panel

--- frontend/src/components/home/HomePanelHackernews.js
+++ frontend/src/components/home/HomePanelHackernews.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Skeleton from "@mui/material/Skeleton";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 import { getHackernewsPosts } from "../../services/hackernews";
 import PostPreviewCard from "../PostPreviewCard";
@@ -10,10 +14,11 @@ import PostPreviewCard from "../PostPreviewCard";
 function HackernewsHome() {
     const [posts, setPosts] = useState([]);
     const [apiError, setApiError] = useState("");
+    const [sortType, setSortType] = useState("best");
 
     useEffect(() => {
         setPosts([]);
-        getHackernewsPosts("best")
+        getHackernewsPosts(sortType)
             .then((res) => {
                 setPosts(res.data);
             })
@@ -30,12 +35,39 @@ function HackernewsHome() {
                     setApiError("Something is wrong with the request.");
                 }
             });
-    }, []);
+    }, [sortType]);
 
     if (apiError) return null;
 
+    const handleSortTypeChange = (event) => {
+        setSortType(event.target.value);
+    };
+
     return (
         <Box>
+            <Box sx={{ maxWidth: 120 }}>
+                <FormControl sx={{ mb: 2, minWidth: 120 }} size="small">
+                    <InputLabel id="sort-type-select-label">Sort By</InputLabel>
+                    <Select
+                        labelId="sort-type-select-label"
+                        id="sort-type-select"
+                        value={sortType}
+                        label="Sort By"
+                        defaultValue="best"
+                        onChange={handleSortTypeChange}
+                        sx={{
+                            borderWidth: "2px",
+                            "& #sort-type-select": {
+                                py: 0.7,
+                            },
+                        }}
+                    >
+                        <MenuItem value={"best"}>Best</MenuItem>
+                        <MenuItem value={"top"}>Top</MenuItem>
+                        <MenuItem value={"ask"}>Ask</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
             <Stack spacing={0}>
                 {(posts.length === 0 ? [...Array(5)] : posts).map((postID, index) =>
                     postID ? (
